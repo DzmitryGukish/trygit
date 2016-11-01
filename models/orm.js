ef16309cd8286613a451abfd034e2636ddb57383
DzmitryGukish <dzmitry.gukish@gmail.com>
2016-11-01 17:36:01 +0300

    test orm

--- models/orm.js
+++ models/orm.js
@@ -12,15 +12,49 @@ var orm = {
     });
   },
 
-  insertRecord: function (req, key, callback) {
+  checkEmail: function (email, callback) {
+    db.get(email, function (err, body, headers) {
+      if (err) return callback(err);
+      return callback(null, body, headers);
+    });
+  },
+
+  listEmail: function (callback) {
+    db.view('emails', 'emails', function (err, body) {
+      if (err) return callback(err);
+      var list = body.rows;
+      return callback(null, list);
+    });
+  },
+
+  insertRecord: function (rec, key, callback) {
     if (2 == arguments.length) {
       callback = key;
       key = null;
     };
-    db.insert(req, key, function (err, body, headers) {
+    db.insert(rec, key, function (err, body, headers) {
       if (err) return callback(err);
       return callback(null, body, headers);
     })
+  },
+
+  insertUser: function (rec, callback) {
+    var key = rec.name;
+    db.get(key, function (err, body, headers) {
+      if (err) {
+        if (err.errid === 'non_200') {
+          // this point to insert
+          db.insert(rec, key, function (errIns, bodyIns, headersIns) {
+            return callback(errIns, bodyIns, headersIns);
+          })
+        } else {
+          // some else errors
+          return callback(err);
+        }
+      } else {
+        return callback(new Error('user exists'), body, headers);
+      }
+    });
   }
 };
 
--- test/test_coonect.js
+++ test/test_coonect.js
@@ -3,14 +3,28 @@ var orm = require('../models/orm.js');
 // console.log(orm);
 // console.log(orm.name);
 
-orm.insertRecord({name: "John Dow", email: "johndow@mail.com"}, function (err, body, headers) {
-  if (err) {
-    if (err.errid == 'non_200') {
-      return console.log("Records key exists");
-    } else {
-      return console.log(err.message);
-    }
-  }
-  console.log("body: ", body, 'ok');
-  console.log("headers: ", headers, 'ok');
+// orm.insertRecord({name: "John Dow", email: "johndow@mail.com"}, "John Dow", function (err, body, headers) {
+//   if (err) {
+//     if (err.errid == 'non_200') {
+//       return console.log("Records key exists");
+//     } else {
+//       return console.log(err.message);
+//     }
+//   }
+//   console.log("body: ", body, 'ok');
+//   console.log("headers: ", headers, 'ok');
+// });
+
+// orm.insertUser({name: "Johan Dow", email: "johandow@mail.com"}, function (err, body, headers) {
+//   console.log("err: ", err, ' .end');
+//   if (body) console.log("body: ", body, ' .end');
+//   if (headers) console.log("headers: ", headers, ' .end');
+// });
+
+orm.listEmail(function (err, list) {
+  if (err) throw err;
+  list.forEach(function (doc) {
+    console.log(doc.key, '//', doc.value);
+  })
+
 });
